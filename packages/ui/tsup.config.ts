818870a8ba9939b081c411085caa84f2b481a041
Carter, Braden <Braden_Carter@bcbst.com>
2025-03-14 17:58:58 +0000

    Updated tsup.config.ts

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
     dts: true,
+    external: ['react'],
     clean: true,
     outDir: 'dist',
     tsconfig: 'tsconfig.json',
