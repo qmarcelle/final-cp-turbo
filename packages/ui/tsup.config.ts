dc8a22f0c321e8252ee09f4fe48813c7033af3aa
Qwynn Marcelle <Qwynn_Marcelle@bcbst.com>
2025-05-23 05:21:55 -0400

    chore: update dependencies in package.json and pnpm-lock.yaml to include esbuild and esbuild-wasm

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,12 +1,18 @@
-import {defineConfig} from 'tsup';
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ['src/index.ts'],
-    format: ['cjs', 'esm'],
-    dts: true,
-    external: ['react'],
-    clean: true,
-    outDir: 'dist',
-    tsconfig: 'tsconfig.json',
-    tsBuildInfo:'dist/.tsbuildinfo'
-})
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  external: ['react'],
+  clean: true,
+  outDir: 'dist',
+  tsconfig: 'tsconfig.json',
+  loader: {
+    '.svg': 'file',
+  },
+  esbuildOptions: (options) => {
+    // Use esbuild-wasm
+    options.platform = 'neutral';
+  },
+});
