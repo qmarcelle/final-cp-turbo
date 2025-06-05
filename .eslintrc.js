860a7ee1cd8bf8208fa594e4aa1a709356af293d
Qwynn Marcelle <qmarcelle@southern.edu>
2025-06-05 09:06:19 -0400

    fix: add React ESLint plugins and complete configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,5 +48,23 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'warn',
       },
     },
+    {
+      files: ['**/*.tsx'],
+      extends: [
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+      ],
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'jsx-a11y/anchor-is-valid': 'off',
+      },
+    },
   ],
 } 
