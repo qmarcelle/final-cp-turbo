944c9973ac1c10f45b024e23c0e31324dff4029e
Qwynn Marcelle <qmarcelle@southern.edu>
2025-06-04 21:55:14 -0400

    Committing outstanding changes (bypassing pre-commit hook)

--- apps/broker-portal/src/app/layout.tsx
+++ apps/broker-portal/src/app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from 'next';
 import '../styles/globals.css';
 
-// import '@fontsource/inter/400.css'; // Regular
-// import '@fontsource/inter/500.css'; // Medium
-// import '@fontsource/inter/600.css'; // SemiBold
-// import '@fontsource/inter/700.css'; // Bold
-
 export const metadata: Metadata = {
   title: 'Broker Portal',
   description: 'BCBST Broker Portal',
